d33e4b12fcfe5a677512e1fc523841980f09b5da
kwhitejr <kwhitejr.edu@gmail.com>
2016-02-13 11:55:41 -1000

    mongo db working properly; drawings saved

--- server.js
+++ server.js
@@ -2,11 +2,25 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost/mongoose-demo');
+
+var drawingSchema = mongoose.Schema({
+  name: String,
+  author: String
+});
+// collection name will get pluralized by mongoose
+var Drawing = mongoose.model('Drawing', drawingSchema);
+
 var app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function (req, res) {
+  var newDrawing = new Drawing({
+    name: 'Foo',
+    author: 'Bar'
+  });
+  newDrawing.save();
   res.send('You done gotten it!');
 });
 
@@ -26,16 +40,11 @@ app.route('/drawings/:id')
     res.send('fix that thing');
   });
 
-// mongoose.connect('mongodb://localhost/test');
 
-// var Cat = mongoose.model('Cat', { name: String });
-
-// var kitty = new Cat({ name: 'Zildjian' });
-// kitty.save(function (err) {
-//   if (err) // ...
-//   console.log('meow');
-// });
-
-var server = app.listen(3000, function() {
-  console.log('Listening to port', server.address().port);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  var server = app.listen(3000, function() {
+    console.log('Listening to port', server.address().port);
+  });
 });
